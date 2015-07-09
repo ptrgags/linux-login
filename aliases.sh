d56943c2712651c9076618db1e2b2c4e9adeedaf
Peter Gagliardi <ptrgags@live.com>
2015-07-09 10:21:13 -0400

    Add gitgraphd alias
    Added a git graph in date order, since sometimes it can be a less
    tangled graph than the gitgraph command by itself

--- aliases.sh
+++ aliases.sh
@@ -55,4 +55,5 @@ alias .....='cd ../../../..'
 #http://stackoverflow.com/questions/1057564/pretty-git-branch-graphs
 # with some minor customizations
 alias gitgraph=$'git log --graph --abbrev-commit --decorate --date=relative --format=format:\'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- [%an]%C(reset)%C(bold yellow)%d%C(reset)\' --all'
-
+#Git Graph in date order
+alias gitgraphd='gitgraph --date-order'
