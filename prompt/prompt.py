b8b3d7d68ddfd1e279335819043594c834450f9e
Peter Gagliardi <ptrgags@live.com>
2015-11-04 19:35:13 -0500

    Add bash variables and 256 color codes to prompt.py
    Added the bash PS1 variables and allow the user to enter
    256-color mode colors to color the prompt.

--- prompt/prompt.py
+++ prompt/prompt.py
@@ -2,28 +2,89 @@
 import sys
 import re
 
-escape='\033[{}m'
-colors8 = {
-	'black': escape.format(30),
-	'red': escape.format(31),
-	'green': escape.format(32),
-	'yellow': escape.format(33),
-	'blue': escape.format(34),
-	'magenta': escape.format(35),
-	'cyan': escape.format(36),
-	'white': escape.format(37)
+#Escape code for a color
+escape='\[\033[{}m\]'
+
+#Special effects
+effects = {
+	'normal': '0',
+	'bold': '1',
+	'underline': '4'
 }
+
+#8-color mode colors
+colors8 = {
+	'black': '30',
+	'red': '31',
+	'green': '32',
+	'yellow': '33',
+	'blue': '34',
+	'magenta': '35',
+	'cyan': '36',
+	'white': '37'
+}
+
+#Special Bash PS1 variables
+bash_vars = {
+	':date:': '\d',
+	':host:': '\h',
+	':hostname:': '\H',
+	':jobs:': '\j',
+	':tty:': '\l',
+	':shell:': '\s',
+	':time24:': '\\t',
+	':time12:': '\T',
+	':time:': '\@',
+	':user:': '\u',
+	':version:': '\\v',
+	':release:': '\V',
+	':cwd:': '\w',
+	':cwdbase:': '\W',
+	':history:': '\!',
+	':command:': '\#',
+	':sigil:': '\$'
+}
+
+def parse_color(color):
+	parts = color.split(';')
+	results = []
+	for color in parts:
+		try:
+			colorNum = int(color)
+			results.append('38;5;{}'.format(colorNum))
+		except ValueError:
+			if color in colors8:
+				results.append(colors8[color])
+			elif color in effects:
+				results.append(effects[color])
+	return ';'.join(results)
+			
 
 if __name__ == '__main__':
 	prompt = sys.argv[1]
+
+	#Ensure we clear the color at the end of the prompt
+	if not prompt.endswith('|0|'):
+		prompt += '|0|'
+
 	colors = sys.argv[2:]
 
-	colors = [colors8.get(color, '') for color in colors]
+	#Replace :<var_name>: with the corresponding bash escape character
+	for key in bash_vars:
+		if key in prompt:
+			prompt = prompt.replace(key, bash_vars[key])
 
+	#Get a list of colors the user specifies
+	colors = [parse_color(color) for color in colors]
+
+	#Replace all the colors with their color codes
 	for i, color in enumerate(colors):
-		placeholder = '\{{{}\}}'.format(i + 1)
-		prompt = re.sub(placeholder, color, prompt)
+		placeholder = '|{}|'.format(i + 1)
+		colorcode = escape.format(color)
+		prompt = prompt.replace(placeholder, colorcode)
 
-	prompt = re.sub('\{0\}', escape.format(0), prompt)
+
+	#Replace all
+	prompt = prompt.replace('|0|', escape.format(0))
 
 	print prompt
